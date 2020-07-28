5de9287b6c75cc4cfa4d91bd0c3808f85f1af688
clock442 <langstonsconnor@gmail.com>
2020-07-28 11:19:43 -0700

    added lab 04 mangic 8 ball, and link

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Lab solutions for full stack bootcamp.
 - [lab 01](./solutions/lab01-turtle.py)
 - [lab 02](./solutions/lab02madlib.py)
 - [lab 03](./solutions/lab03grading.py)
+- [lab 04](./solutions/lab04magic8ball.py)
---
+++ lab04magic8ball.py
@@ -0,0 +1,14 @@
+import random
+answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes – definitely', 
+'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes Signs point to yes', 
+'Reply hazy', 'try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 
+'Concentrate and ask again', 'Dont count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 
+'Very doubtful']
+
+x = True
+while x == True:
+    input('Ask me a question.')
+    print(random.choice(answers))
+    y = input('Would you like to play again?: y/n')
+    if y == 'n':
+        x = False
