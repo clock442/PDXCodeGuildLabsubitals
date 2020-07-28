70cf4a4536ce84bd2f07e0d626b7836776d64fea
clock442 <langstonsconnor@gmail.com>
2020-07-28 13:56:06 -0700

    fixed lab 05

--- solutions/lab05emotegen.py
+++ solutions/lab05emotegen.py
@@ -2,12 +2,12 @@ import random
 eyes = ['=', ';', ':','8',]
 nose = [' ', '-', '^', '>',]
 mouth = ['O', 'I', '0', '[', ']', ')', '(', '}', '{', '/', 'l']
-i = 0
 
 x = True
 while x:
     question = input('Would you like to make 5 random faces? y/n:   ')
     if question == 'y':
+        i = 0
         while i < 5:
             print(random.choice(eyes) + random.choice(nose) + random.choice(mouth))
             i += 1
@@ -15,5 +15,3 @@ while x:
     if playagain == 'n':
         print('Bye Bye.')
         x = False
-    question = ''
-    i = 0
