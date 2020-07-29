8a87af758400894acc0c7b4ef75db89c14870aaf
clock442 <langstonsconnor@gmail.com>
2020-07-29 12:20:52 -0700

    version 1.2

--- solutions/lab07-rock_paper_scissors.py
+++ solutions/lab07-rock_paper_scissors.py
@@ -10,10 +10,10 @@ while x == True:
     print('Lets play rock, paper, scissors')
     player_choice = input('Choose rock, paper, or scissors.:   ')
     player_choice = player_choice.lower()
+    result = 'Please make a valid choice.'
     if player_choice in choice:
         comp_hand = rand_comp_choice()
         print(f'The computer chose {comp_hand}.')
-        result = ''
         if player_choice == 'rock':
             if comp_hand == 'scissors':
                 result = 'You won!'
@@ -35,9 +35,7 @@ while x == True:
                 result = 'You lost'
             elif comp_hand == 'scissors':
                 result = 'We tied.'
-    else:
-        print('Please make a valid choice.')
-        continue
+
     print(result)
     play_again = input('Would you like to play again? y/n:   ')
     if play_again == 'n':
