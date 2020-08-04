1a023c0c724737a49b1316bd73b91e870ecdd966
clock442 <langstonsconnor@gmail.com>
2020-08-04 13:53:09 -0700

    lab 10 version 2

--- solutions/lab10pvs.py
+++ solutions/lab10pvs.py
@@ -5,8 +5,23 @@ def peaks(data):
 def valleys(data):
     return [i for i in range(1, len(data)-1) if data[i-1] > data[i] and data[i] < data[i+1]]
 
+def chart_builder(data):
+    greatest_number = max(data)
+    for i in range(0, greatest_number):
+        graph_list = []
+        for j in range(len(data)):
+            if data[j] < greatest_number:
+                graph_list.append('  ')
+            else:
+                graph_list.append(' X')
+        graph_list = ''.join(graph_list)
+        print(graph_list)
+        greatest_number -= 1
+
 def peaks_and_valleys(data):
+    chart_builder(data)
     print(f'The peaks were at these points {peaks(data)} and the valleys were at these points {valleys(data)}.')
+
 
 
 
