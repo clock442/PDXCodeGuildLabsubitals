71fd91b12da68ad2ef502c217d2d4b3c4812db0a
clock442 <langstonsconnor@gmail.com>
2020-08-05 14:41:23 -0700

    ...

--- solutions/lab11makechange.py
+++ solutions/lab11makechange.py
@@ -7,6 +7,7 @@ coins = [
 while True:
     coin_amounts = []
     total_cents = float(input('Enter a dollar amount:   '))*100
+    total_cents = round(total_cents)
     for i in range(len(coins)):
         current_coin_value = coins[i][1]
         if total_cents >= current_coin_value:
