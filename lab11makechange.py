62755e5f3d466478562584d72595f7883456bed6
clock442 <langstonsconnor@gmail.com>
2020-08-05 11:30:24 -0700

    lab11 ver2

--- lab11makechange.py
+++ lab11makechange.py
@@ -1,13 +1,34 @@
-import string
-coins = [25, 10, 5, 1]
+# import string
+# coins = [25, 10, 5, 1]
+# while True:
+#     coin_amounts = [0, 0, 0, 0]
+#     total_cents = float(input('Enter a dollar amount:   '))*100
+#     for i in range(len(coins)):
+#         if total_cents >= coins[i]:
+#             num_coins = total_cents//coins[i]
+#             coin_amounts[i] += num_coins
+#             total_cents -= coins[i]*num_coins
+#     print(f'You would have {int(coin_amounts[0])} quarters, {int(coin_amounts[1])} dimes, {int(coin_amounts[2])} nickels, and {int(coin_amounts[3])} pennies.')
+#     repeat = input('Would you like to make more change? y/n:   ')
+#     if repeat != 'y':
+#         print('Bye Bye')
+#         break
+
+coins = [
+    ('quarter', 25),
+    ('dime', 10),
+    ('nickel', 5),
+    ('penny', 1)
+]
 while True:
     coin_amounts = [0, 0, 0, 0]
     total_cents = float(input('Enter a dollar amount:   '))*100
     for i in range(len(coins)):
-        if total_cents >= coins[i]:
-            num_coins = total_cents//coins[i]
+        current_value = coins[i][1]
+        if total_cents >= current_value:
+            num_coins = total_cents//current_value
             coin_amounts[i] += num_coins
-            total_cents -= coins[i]*num_coins
+            total_cents -= current_value*num_coins
     print(f'You would have {int(coin_amounts[0])} quarters, {int(coin_amounts[1])} dimes, {int(coin_amounts[2])} nickels, and {int(coin_amounts[3])} pennies.')
     repeat = input('Would you like to make more change? y/n:   ')
     if repeat != 'y':
