263731dc93c4ae61391d83b40c395c638bc74d87
clock442 <langstonsconnor@gmail.com>
2020-08-14 15:31:34 -0700

    update

--- solutions/lab17raindata.py
+++ solutions/lab17raindata.py
@@ -12,10 +12,9 @@ for i in range(len(rain_list)):
     if len(rain_list[i]) > 3:
         appen_rain_list.append((rain_list[i], rain_list[i+1]))
 
-# make a list of dictionaries with date as the first key and rain as the second
+# make a list of dictionaries with date as the first key and rain as the second each dictionary represents one day
 fixed_list = []
 for i in range(39, len(appen_rain_list)):
-    # date = datetime.strptime(appen_rain_list[i][0], '%d-%b-%Y')
     fixed_list.append({'date':appen_rain_list[i][0], 'rain':appen_rain_list[i][1]})
 
 # removing '-' from the list and changing 'rain': strings into 'rain': integers
@@ -31,9 +30,9 @@ all_rain = 0
 for j in range(len(fixed_list)):
     all_rain += fixed_list[j]['rain']
 average_daily_rain = all_rain/total_days
-print(f'The average daily rainfall in the Portland area is {average_daily_rain*.01} inches.')
+print(f'The average daily rainfall in the Cottrell School area is {average_daily_rain*.01} inches.')
 
-# finding the variance
+# finding the variance and standard deviation
 root_list = []
 for i in range(len(fixed_list)):
     zut = (fixed_list[i]['rain'] - average_daily_rain)**2
@@ -56,12 +55,12 @@ for k in range(len(fixed_list)):
 print(f'The date of the the day with the most rainfall in one day is {most_rain_date} on which {most_rain_in_day*.01} inches of rain fell.')
 
 # plot a graph with dates as the x axis and rain levels as the y axis
-x_values = []
-y_values = []
+all_dates = []
+rain_data_day = []
 for i in range(len(fixed_list)):
     n_date = datetime.strptime(fixed_list[i]['date'],'%d-%b-%Y')
-    x_values.append(n_date)
-    y_values.append(fixed_list[i]['rain'])
-plt.plot(x_values, y_values)
+    all_dates.append(n_date)
+    rain_data_day.append(fixed_list[i]['rain'])
+plt.plot(all_dates, rain_data_day)
 plt.show()
 
