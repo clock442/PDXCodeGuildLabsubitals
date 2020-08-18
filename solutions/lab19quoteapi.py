3d0f204eb2414c5b6ec6f4260b329e05f70aee0c
clock442 <langstonsconnor@gmail.com>
2020-08-18 14:44:56 -0700

    lab 19 v2 fixed

--- solutions/lab19quoteapi.py
+++ solutions/lab19quoteapi.py
@@ -1,23 +1,23 @@
 import requests
 import json
 
-# response = requests.get('https://favqs.com/api/qotd')
-
-# data = response.json()
-# print(f'''{data['quote']['author']}: "{data['quote']['body']}"''')
 while True:
+    page = 1
     keyword = input('enter a keyword to search for quotes:  ')
-    page = int(input('What page do you want:  '))
-    # if page != int:
-    #     print('not a number')
-    #     continue
-    headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
-    params = {'filter': keyword, 'page': page, 'Accept': 'application/json'}
-    response = requests.get('https://favqs.com/api/quotes', headers=headers, params=params)
-    data = response.json()
-    # elif page > len(data['quotes']):
-    #     print('not enough pages, pick a lower number')
-
-    # print(data)
-    for i in range(len(data['quotes'])):
-        print(f'''{data['quotes'][i]['author']}: "{data['quotes'][i]['body']}"''')
+    keyword = keyword.lower().strip()
+    while True:
+        headers = {'Authorization': 'Token token="855df50978dc9afd6bf86579913c9f8b"'}
+        params = {'filter': keyword, 'page': page, 'Accept': 'application/json'}
+        response = requests.get('https://favqs.com/api/quotes', headers=headers, params=params)
+        data = response.json()
+        for i in range(len(data['quotes'])):
+            print(f'''{data['quotes'][i]['author']}: "{data['quotes'][i]['body']}"''')
+        nxt_page = input('Would you like to view the next page? y/n:  ')
+        if nxt_page == 'n':
+            break
+        else:
+            page += 1
+    replay = input('Would you like to continue searching for quotes? y/n:  ')
+    if replay == 'n':
+        break
+print('Bye Bye')
