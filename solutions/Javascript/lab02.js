362cc079c65fc56b8f4e483a57ecf744e5e46758
clock442 <langstonsconnor@gmail.com>
2020-08-27 14:08:20 -0700

    lab02 w/ dom manip

--- solutions/Javascript/lab02.js
+++ solutions/Javascript/lab02.js
@@ -22,17 +22,32 @@ let matrix = {
         scissors: 'Tie'
     }
 }
-while (true) {
-    alert('Welcome to Rock, Paper, Scissors. I will pick a random choice. Try and beat me.')
-    let userChoice = prompt('Chose rock, paper, scissors')
-    if (!choice.includes(userChoice)){
-        alert('Enter something valid.')
-        continue
-    }
+
+let user_input = document.querySelector('#user_input')
+let play_bt = document.querySelector('#play_bt')
+let output_div = document.querySelector('#output_div')
+
+play_bt.onclick = function() {
     let computerChoice = getComputerChoice()
-    alert('You ' + matrix[userChoice][computerChoice])
-    let repeat = prompt('Would you like to play again? y/n')
-    if (repeat == 'n'){
-        break
+    let hand = user_input.value;
+    if (!choice.includes(hand)){
+        output_div.innerText = 'Enter something Valid'
+    }else {
+        output_div.innerText = 'You ' + matrix[hand][computerChoice]
     }
 }
+
+// while (true) {
+//     alert('Welcome to Rock, Paper, Scissors. I will pick a random choice. Try and beat me.')
+//     let userChoice = prompt('Chose rock, paper, scissors')
+//     if (!choice.includes(userChoice)){
+//         alert('Enter something valid.')
+//         continue
+//     }
+//     let computerChoice = getComputerChoice()
+//     alert('You ' + matrix[userChoice][computerChoice])
+//     let repeat = prompt('Would you like to play again? y/n')
+//     if (repeat == 'n'){
+//         break
+//     }
+// }
