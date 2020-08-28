e84b3c5614da7ee13e45c70c5450d5d6549eac89
clock442 <langstonsconnor@gmail.com>
2020-08-28 15:55:06 -0700

    .

--- solutions/Javascript/lab03.js
+++ solutions/Javascript/lab03.js
@@ -12,11 +12,34 @@ run_bt.onclick = function() {
         if (inputText2[i] ==' '){
             outputText += ' '
         }else if (!alphabet.includes(inputText2[i])){
-            outputText += ''
-        }else if (alphabet.includes(inputText2[i])){
+            continue
+        }else {
             console.log(alphabet.indexOf(inputText2[i]))
-            outputText += alphabet[(alphabet.indexOf(inputText2[i]) + rotValue)%alphabet.length]
+            outputText += alphabet[Math.abs((alphabet.indexOf(inputText2[i]) + rotValue))%alphabet.length]
         }
     }
     outputDiv.innerText = outputText
+}
+
+let rotNumBack = document.querySelector('#rotNumBack')
+let inputTextBack = document.querySelector('#inputTextBack')
+let run_bt_back = document.querySelector('#run_bt_back')
+let outputDivBack = document.querySelector('#outputDivBack')
+let alphabetBack = 'zyxwvutsrqponmlkjihgfedcba'
+
+run_bt_back.onclick = function() {
+    let outputText = ''
+    let inputText2 = inputTextBack.value.toLowerCase()
+    let rotValue = parseInt(rotNumBack.value)
+    for (i in inputText2){
+        if (inputText2[i] ==' '){
+            outputText += ' '
+        }else if (!alphabetBack.includes(inputText2[i])){
+            continue
+        }else {
+            console.log(alphabetBack.indexOf(inputText2[i]))
+            outputText += alphabetBack[Math.abs((alphabetBack.indexOf(inputText2[i]) + rotValue))%alphabet.length]
+        }
+    }
+    outputDivBack.innerText = outputText
 }
