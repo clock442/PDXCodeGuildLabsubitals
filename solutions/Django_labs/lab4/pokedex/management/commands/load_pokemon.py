d810f8456423adc77592416753fcc0bedde3dd58
clock442 <langstonsconnor@gmail.com>
2020-09-30 16:29:14 -0700

    .

---
+++ solutions/Django_labs/lab4/pokedex/management/commands/load_pokemon.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+import json
+from pokedex.models import Pokemon, PokemonType
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        with open('pokedex/management/commands/pokemon.json', 'r') as poketext:
+            pokejson = poketext.read()
+        pokedata = json.loads(pokejson)
+        for pokemon_data in pokedata['pokemon']:
+            number = pokemon_data['number']
+            name = pokemon_data['name']
+            height = pokemon_data['height']
+            weight = pokemon_data['weight']
+            image_front = pokemon_data['image_front']
+            image_back = pokemon_data['image_back']
+            type_names = pokemon_data['types']
+
+            pokemon = Pokemon(number=number, name=name, height=height, weight=weight, image_front=image_front, image_back=image_back)
+            pokemon.save()
+
+            for type_name in type_names:
+                pokemon_type, created = PokemonType.objects.get_or_create(
+                    name=type_name
+                )
+                pokemon.types.add(pokemon_type)
+
+            pokemon.save()
---
+++ solutions/Django_labs/lab4/pokedex/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class PokemonType(models.Model):
+    name = models.CharField(max_length=10)
+    def __str__(self):
+        return self.name
+
+class Pokemon(models.Model):
+    number = models.IntegerField()
+    name = models.CharField(max_length=30)
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    image_front = models.CharField(max_length=300)
+    image_back = models.CharField(max_length=300)
+    types = models.ManyToManyField(PokemonType, related_name='pokemons')
+    def __str__(self):
+        return self.name
