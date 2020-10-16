9a7be9ab82982c352850c1e3cfee82d09a04523e
clock442 <langstonsconnor@gmail.com>
2020-10-16 10:26:28 -0700

    .

--- solutions/Django_labs/lab6/lab6/urls.py
+++ solutions/Django_labs/lab6/lab6/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('library', include('library.urls'))
+    path('library/', include('library.urls'))
 ]
