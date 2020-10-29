e5db231c77b4364ab37dc3df9f6dad70d57d210c
clock442 <langstonsconnor@gmail.com>
2020-10-29 10:01:50 -0700

    .

--- solutions/Django_labs/lab5/lab5/settings.py
+++ solutions/Django_labs/lab5/lab5/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from . import local_settings
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gpw@$%wf(loejafryvt^1d!sm%x_@+j_b7$9jzj_^*s=lf7ji#'
+SECRET_KEY = local_settings.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
